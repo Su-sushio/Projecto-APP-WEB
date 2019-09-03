0c2a38a4c454bde7478bf4cc197bde45354c33d2
Su-sushio <susana_s_santos@hotmail.com>
2019-09-03 15:20:27 +0100

    Implementação Login e Registo de Utilizador
    Comandos:
    
    -php artisan:auth
    - php artisan migrate
    - php artisan make: seed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>»»»» CHECK.YOUR.PRODUCTS.LIST ««««</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -45,7 +45,7 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 64px;
             }
 
             .links > a {
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    »»»» CHECK.YOUR.PRODUCTS.LIST ««««
                 </div>
 
                 <div class="links">
