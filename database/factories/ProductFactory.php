9388efbcbe044489ed370a5431cc20efecd77580
MarciaSilveira <53255843+MarciaSilveira@users.noreply.github.com>
2019-09-04 15:17:07 +0100

    progresso lista de produtos 2

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
+
+            "name"=>"Televisão LG",
+            "notes"=>"32 polegadas",
+            "link"=>"",
+            "price"=>"500"
+            
+        ];
+    });
         //
     ];
 });
